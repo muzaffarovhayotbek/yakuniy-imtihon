c30c9e9b6d0803190506357aa64445ab52f5860d
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-28 23:08:35 +0500

    add

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,7 +23,7 @@ function Modal() {
   } , [data])
 
   return (
-    <dialog id="my_modal_1" className="modal sm:flex">
+    <dialog id="my_modal_1" className="modal">
       <div className="modal-box">
         <h3 className="font-bold text-black mb-4">Reset password</h3>
         
